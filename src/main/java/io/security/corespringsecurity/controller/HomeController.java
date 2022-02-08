f213628924afc2b3801696bcd3bc10576c3281f3
ndy2 <emrdbs12@gmail.com>
2022-02-08 22:26:35 +0900

    4-1
    project setting

--- src/main/java/io/security/corespringsecurity/controller/HomeController.java
+++ src/main/java/io/security/corespringsecurity/controller/HomeController.java
@@ -1,11 +1,11 @@
 package io.security.corespringsecurity.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 	
 	@GetMapping(value="/")
